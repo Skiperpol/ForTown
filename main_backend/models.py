e545b4233e981805563e61e9f323c2a3b20b0a51
Skiperpol <skiperpol1@gmail.com>
2022-04-10 23:27:46 +0200

    ForTown2

--- main_backend/models.py
+++ main_backend/models.py
@@ -17,8 +17,8 @@ class Event(models.Model):
     deadline = models.DateField()
     type_of_event = models.CharField(max_length=30, choices=TITLE_CHOICES)
     link_do_miejsca_wydarzenia =  models.URLField(max_length = 200)
-    x_miejsca = models.FloatField()
-    y_miejsca = models.FloatField()
+    x_miejsca = models.FloatField(null=True)
+    y_miejsca = models.FloatField(null=True)
 
     def __str__(self):
         return self.title
